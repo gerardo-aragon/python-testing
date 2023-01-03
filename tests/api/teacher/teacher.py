eeccd39ef26265d4e77992ee7a4cc1745457c32d
gerardo-aragon <gerardo.aragon@edify.cr>
2023-01-03 09:34:14 -0600

    Finish teacher module

---
+++ tests/api/teacher/teacher.py
@@ -0,0 +1,192 @@
+import json
+import time
+
+from utils.api_helper import *
+from src.endpoints.auth.auth import *
+from src.endpoints.teacher.teacher import *
+from src.endpoints.users.users import *
+from utils.asserts import *
+from pytest_check import check
+from utils.payloads.create_teacher import *
+
+
+def create_random_field():
+    time.sleep(1)
+    epoch = datetime.datetime.today().strftime('%s')
+    user_name = epoch
+    return user_name
+
+
+def create_parametrize_data():
+    cedula_id = create_random_field()
+    email = "user_teacher_" + create_random_field() + "@gmail.com"
+    user_name = "user_teacher_" + create_random_field()
+    return cedula_id, email, user_name
+
+
+@pytest.mark.usefixtures("auth")
+class TestTeacherApi:
+
+    def test_01_create_teacher_successful(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher
+        cedula_id, email, user_name = create_parametrize_data()
+        response_data = teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # verify if cedulaId is present
+        dictionary = response_data['created'][0]
+        check.is_true(is_key_present(dictionary, "cedulaId"))
+
+    def test_02_create_existing_teacher_id(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Create teacher with existing cedula_id
+        new_email = "user_teacher_" + create_random_field() + "@gmail.com"
+        new_user_name = "user_teacher_" + create_random_field()
+        response_data = teacher.post_create_teacher(auth, 201, cedula_id, new_email, new_user_name)
+        error_message = response_data['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+
+    def test_03_create_existing_teacher_user(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Create teacher with existing cedula_id
+        new_cedula_id = create_random_field()
+        new_email = "user_teacher_" + create_random_field() + "@gmail.com"
+        response_data = teacher.post_create_teacher(auth, 201, new_cedula_id, new_email, user_name)
+        error_message = response_data['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+
+    def test_04_create_existing_teacher_email(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Create teacher with existing cedula_id
+        new_cedula_id = create_random_field()
+        new_user_name = "user_admin_" + create_random_field()
+        response_data = teacher.post_create_teacher(auth, 201, new_cedula_id, email, new_user_name)
+        error_message = response_data['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+
+    def test_05_get_all_teacher_users(self, auth):
+        teacher = TeacherApi()
+        # obtain all teacher users
+        response_data = teacher.get_teacher_users(auth, 200)
+
+        # get TEACHER role
+        teacher_rol = response_data['data'][0]['role']
+
+        # check if the obtained rol is equal to TEACHER
+        check.equal(teacher_rol,"TEACHER")
+
+        # check if cedulaId is present
+        dictionary = response_data['data'][0]
+        check.is_true(is_key_present(dictionary, "cedulaId"))
+
+    def test_06_get_specific_user(self, auth):
+        teacher = TeacherApi()
+        user = UsersApi()
+
+        # obtain all teacher uses
+        response_data = teacher.get_teacher_users(auth, 200)
+
+        # get teacher cedulaId
+        admin_cedula_id = response_data['data'][0]['cedulaId']
+
+        # get specific teacher
+        dictionary = user.get_user(auth, 200, admin_cedula_id)
+
+        # Verify if the response body contains ....
+        check.is_true(is_key_present(dictionary, "email"))
+        check.is_true(is_key_present(dictionary, "userName"))
+
+
+    def test_07_edit_teacher_user_successful(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher if there are no teachers
+        cedula_id, email, user_name = create_parametrize_data()
+        response_data = teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Get cedula_id
+        teacher_id = response_data['created'][0]['cedulaId']
+
+        # Create new teacher data
+        new_email = "edited_teacher_" + create_random_field() + "@gmail.com"
+        new_user_name = "edited_teacher_" + create_random_field()
+
+        # Edit the teacher
+        dictionary = teacher.put_edit_teacher(auth, 200, teacher_id, new_email, new_user_name)
+
+        # Verify if the response body contains ....
+        check.is_true(is_key_present(dictionary, "email"))
+        check.is_true(is_key_present(dictionary, "userName"))
+
+        # Verify if the data was edited correctly
+        check.equal(dictionary['email'], new_email)
+
+
+    def test_08_edit_teacher_existing_email(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher if there are no teachers
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Create new teacher data
+        new_user_name = "edited_teacher_" + create_random_field()
+
+        # Edit the teacher
+        dictionary = teacher.put_edit_teacher(auth, 200, cedula_id, email, new_user_name)
+        error_message = dictionary['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+    def test_09_edit_teacher_existing_user_name(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher if there are no teachers
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Create new teacher data
+        new_email = "edited_" + create_random_field() + "@gmail.com"
+
+        # Edit the teacher
+        dictionary = teacher.put_edit_teacher(auth, 200, cedula_id, new_email, user_name)
+        error_message = dictionary['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+    def test_10_delete_teacher_user(self, auth):
+        teacher = TeacherApi()
+
+        # Create teacher if there are no admins
+        cedula_id, email, user_name = create_parametrize_data()
+        teacher.post_create_teacher(auth, 201, cedula_id, email, user_name)
+
+        # Delete the teacher
+        dictionary = teacher.delete_teacher_user(auth, 200, cedula_id)
+
+        # Validate the deleted id
+        deleted_id = dictionary['id']
+        check.equal(deleted_id, int(cedula_id))
+        check.is_true(is_key_present(dictionary, "id"))
+
+
+
+
+
