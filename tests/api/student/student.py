468a27cbfb73c79ead0447b20e2908851e36e78c
gerardo-aragon <gerardo.aragon@edify.cr>
2023-01-23 09:20:30 -0600

    Finish student endpoints

---
+++ tests/api/student/student.py
@@ -0,0 +1,133 @@
+import json
+import time
+
+from utils.api_helper import *
+from src.endpoints.auth.auth import *
+from src.endpoints.student.student import *
+from src.endpoints.users.users import *
+from utils.asserts import *
+from pytest_check import check
+from utils.payloads.create_student import *
+
+
+def create_random_field():
+    time.sleep(1)
+    epoch = datetime.datetime.today().strftime('%s')
+    user_name = epoch
+    return user_name
+
+
+def create_parametrize_data():
+    cedula_id = create_random_field()
+    email = "user_student_" + create_random_field() + "@gmail.com"
+    user_name = "user_student_" + create_random_field()
+    return cedula_id, email, user_name
+
+
+@pytest.mark.usefixtures("auth")
+class TestStudentApi:
+
+    def test_01_create_student_successful(self, auth):
+        student = StudentApi()
+
+        # Create student
+        cedula_id, email, user_name = create_parametrize_data()
+        response_data = student.post_create_student(auth, 201, cedula_id)
+
+        # verify if id is present
+        dictionary = response_data['created'][0]
+        check.is_true(is_key_present(dictionary, "id"))
+
+        # Delete the student to avoid unnecessary data
+        student.delete_student_user(auth, 200, cedula_id)
+
+
+    def test_02_create_existing_student_id(self, auth):
+        student = StudentApi()
+
+        # Create student
+        cedula_id, email, user_name = create_parametrize_data()
+        student.post_create_student(auth, 201, cedula_id)
+        
+        # Create student with existing cedula_id
+        dictionary = student.post_create_student(auth, 201, cedula_id)
+        error_message = dictionary['noCreated'][0]['error']
+        check.equal(error_message, "El usuario ya existe")
+
+        # Delete the student to avoid unnecessary data
+        student.delete_student_user(auth, 200, cedula_id)
+        student.delete_student_user(auth, 200, dictionary['created'][0]['id'])
+
+
+    def test_03_get_all_student_users(self, auth):
+        student = StudentApi
+
+        # Get all students
+        response_data = student.get_student_users(auth, 200)
+
+        # Validate if the response has cedulaId and student
+        dictionary = response_data['data'][0]
+        check.is_true(is_key_present(dictionary, "cedulaId"))
+        check.is_true(is_key_present(dictionary, "student"))
+
+
+    def test_04_get_specific_student(self, auth):
+        student = StudentApi()
+
+        # Create student
+        cedula_id, email, user_name = create_parametrize_data()
+        student.post_create_student(auth, 201, cedula_id)
+        
+        # Get specific student
+        dictionary = student.get_specific_student_user(auth, 200, cedula_id)
+
+        # Validate the response body.
+        check.is_true(is_key_present(dictionary, "id"))
+        check.is_true(is_key_present(dictionary['studentGroup'], "groupId"))
+        check.is_true(is_key_present(dictionary['studentGroup']['group'], "managerId"))
+
+        # Delete the student to avoid unnecessary data
+        student.delete_student_user(auth, 200, cedula_id)
+
+
+    def test_05_edit_student(self, auth):
+        student = StudentApi()
+
+        # Create student
+        cedula_id, email, user_name = create_parametrize_data()
+        student.post_create_student(auth, 201, cedula_id)
+        
+        # Edit student
+        dictionary = student.put_edit_student(auth, 200, cedula_id)
+
+        # Verify if the response body contains ....
+        payload = set_edit_student_payload(cedula_id)
+        check.is_true(is_key_present(dictionary, "id"))
+        check.is_true(is_key_present(dictionary, "tutor"))
+        check.equal(int(cedula_id), int(payload["id"]))
+
+        # Delete the student to avoid unnecessary data
+        student.delete_student_user(auth, 200, cedula_id)
+
+
+    def test_06_delete_student(self, auth):
+        student = StudentApi()
+
+        # Create student
+        cedula_id, email, user_name = create_parametrize_data()
+        student.post_create_student(auth, 201, cedula_id)
+
+        # Delete student
+        dictionary = student.delete_student_user(auth, 200, cedula_id)
+        
+        # Verify if the response body contains...
+        check.is_true(is_key_present(dictionary, "id"))
+        check.equal(int(cedula_id), int(dictionary["id"]))
+        
+        
+        
+
+
+
+
+
---
+++ utils/payloads/create_student.py
@@ -0,0 +1,29 @@
+import datetime
+import time
+
+def set_create_student_payload(cedula_id):
+    payload_create_student = [{
+        "id": int(cedula_id),
+        "firstName": "Student",
+        "lastName": "Upre",
+        "birthdate": "09/11/1992",
+        "tutor": "Tutor",
+        "phone": 60606060,
+        "address": "Dirección 01",
+        "group": "7-1 (No eliminar)"
+    }]
+    return payload_create_student
+
+def set_edit_student_payload(cedula_id):
+    payload_edit_student = {
+        "id": int(cedula_id),
+        "firstName": "Student",
+        "lastName": "Edited",
+        "birthdate": "09/11/1990",
+        "tutor": "Tutor edited",
+        "phone": 70707070,
+        "address": "Dirección edited",
+        "group": "7-1 (No eliminar)"
+    }
+    return payload_edit_student
+
